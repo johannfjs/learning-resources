aff6b5dce0a89b74536bfa383445d300c8c679e8
Eduardo Hoyos <edu.hoyos92@gmail.com>
2025-11-03 18:47:26 -0500

    Testing for fork

---
+++ Architecture/arch-test/arch-test/arch_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  arch_testApp.swift
+//  arch-test
+//
+//  Created by Eduardo Hoyos on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct arch_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
